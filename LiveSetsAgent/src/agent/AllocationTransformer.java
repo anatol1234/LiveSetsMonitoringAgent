4976b7b5ad5076c087595d765be3886bedbcdddb
Stefan <stefandotgasser@gmail.com>
2017-05-30 14:09:32 +0200

    Support for Arrays and MuliArrays missing: Primitive Arrays
    SG

--- LiveSetsAgent/src/agent/AllocationTransformer.java
+++ LiveSetsAgent/src/agent/AllocationTransformer.java
@@ -12,6 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import alloclogger.AllocationLogger;
+import com.sun.org.apache.bcel.internal.generic.ICONST;
 import javassist.bytecode.BadBytecode;
 import javassist.bytecode.Bytecode;
 import javassist.bytecode.ClassFile;
@@ -51,7 +52,7 @@ class AllocationTransformer implements ClassFileTransformer{
 	private void transform(ClassFile clazz) throws BadBytecode{				
 		@SuppressWarnings("unchecked")
 		List<MethodInfo> methods = clazz.getMethods();
-		for(MethodInfo method : methods) {			
+		for(MethodInfo method : methods) {
 			transform(clazz.getName(), method);
 		}
 	}
@@ -66,6 +67,16 @@ class AllocationTransformer implements ClassFileTransformer{
 				if(opcode == Opcode.NEW) {
 					int objectrefIndex = iterator.u16bitAt(bci+1);
 					iterator.insert(bci, emitAllocationDetected(clazz, method, bci, objectrefIndex));
+				} else if(opcode == Opcode.ANEWARRAY){
+					int objectrefIndex = iterator.u16bitAt(bci+1);
+					iterator.insert(bci, emitArrayAllocationDetected(clazz, method, bci, objectrefIndex));
+				} else if(opcode == Opcode.MULTIANEWARRAY){
+					int objectrefIndex = iterator.u16bitAt(bci+1);
+					int count = iterator.byteAt(bci+3);
+					iterator.insert(bci, emitMultiArrayAllocationDetected(clazz, method, bci, objectrefIndex, count));
+				} else if(opcode == Opcode.NEWARRAY){
+					int aType = iterator.byteAt(bci+1);
+					iterator.insert(bci, emitPrimitiveArrayAllocationDetected(clazz, method, bci, aType));
 				}
 			}
 			code.computeMaxStack();
@@ -82,6 +93,44 @@ class AllocationTransformer implements ClassFileTransformer{
 		bytecode.addIconst(method.getLineNumber(newopbci));
 		bytecode.addInvokevirtual(AllocationLogger.class.getName(), "allocDetected", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;II)V");
 		return bytecode.get();
-	}	
+	}
+
+	private byte[] emitArrayAllocationDetected(String clazz, MethodInfo method, int newopbci, int objectrefIndex) {
+		Bytecode bytecode = new Bytecode(method.getConstPool());
+		bytecode.addInvokestatic(AllocationLogger.class.getName(), "getAllocationLogger", "()L" + AllocationLogger.class.getPackage().getName() + "/AllocationLogger;");
+		bytecode.addLdc(method.getConstPool().getClassInfo(objectrefIndex));
+		bytecode.addLdc(clazz);
+		bytecode.addLdc(method.getName());
+		bytecode.addIconst(newopbci);
+		bytecode.addIconst(method.getLineNumber(newopbci));
+		bytecode.addInvokevirtual(AllocationLogger.class.getName(), "arrayAllocDetected", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;II)V");
+		return bytecode.get();
+	}
+
+	private byte[] emitMultiArrayAllocationDetected(String clazz, MethodInfo method, int newopbci, int objectrefIndex, int dims) {
+		Bytecode bytecode = new Bytecode(method.getConstPool());
+		bytecode.addInvokestatic(AllocationLogger.class.getName(), "getAllocationLogger", "()L" + AllocationLogger.class.getPackage().getName() + "/AllocationLogger;");
+		bytecode.addLdc(method.getConstPool().getClassInfo(objectrefIndex));
+		bytecode.addLdc(clazz);
+		bytecode.addLdc(method.getName());
+		bytecode.addIconst(newopbci);
+		bytecode.addIconst(method.getLineNumber(newopbci));
+		bytecode.addIconst(dims);
+		bytecode.addInvokevirtual(AllocationLogger.class.getName(), "mutliArrayAllocDetected", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;III)V");
+		return bytecode.get();
+	}
+
+	private byte[] emitPrimitiveArrayAllocationDetected(String clazz, MethodInfo method, int newopbci, int aType) {
+		Bytecode bytecode = new Bytecode(method.getConstPool());
+		bytecode.addInvokestatic(AllocationLogger.class.getName(), "getAllocationLogger", "()L" + AllocationLogger.class.getPackage().getName() + "/AllocationLogger;");
+		//TODO SG type of primitive Array missing
+		//bytecode.addLdc(method);
+		bytecode.addLdc(clazz);
+		bytecode.addLdc(method.getName());
+		bytecode.addIconst(newopbci);
+		bytecode.addIconst(method.getLineNumber(newopbci));
+		bytecode.addInvokevirtual(AllocationLogger.class.getName(), "arrayAllocDetected", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;II)V");
+		return bytecode.get();
+	}
 	
 }
